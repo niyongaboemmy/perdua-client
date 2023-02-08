2e0a2d096d2764eb8ff76664b8e79de6a1b4904b
niyongaboemmy <emmanuelniyongabo44@gmail.com>
2023-02-08 15:27:45 +0200

    Changes about section to WALL...

--- components/HomepageComponents/AboutUs.tsx
+++ components/HomepageComponents/AboutUs.tsx
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsArrowRight } from "react-icons/bs";
 import { MdReadMore } from "react-icons/md";
-import AboutImage from "../../assets/about.png";
+import AboutImage from "../../assets/NewAbout.png";
 import Container from "../Container/Container";
 
 interface AboutUsProps {}
@@ -27,20 +27,48 @@ export class AboutUs extends Component<AboutUsProps, AboutUsState> {
                 </div>
               </div>
               <div className="col-span-12 md:col-span-7 p-4 flex flex-col justify-center">
-                <div className="text-4xl font-bold mb-4">
-                  About Perdua Publishers
+                <div className="text-4xl font-bold mb-4">WALL CHARTS</div>
+                <div className="mb-5 text-gray-500 text-justify">
+                  Perdua Publishers has also designed wall charts for
+                  pre-primary and ECD classrooms. Wall charts make a big impact
+                  in children’s understanding of basic concepts, making learning
+                  near-to-real and reinforce lessons. Wall charts are great
+                  teaching aids. Based on REB CBC, these charts are approved to
+                  be used in schools. The wall charts developed are:
+                </div>
+                <div className="mb-5 grid grid-cols-12 gap-2">
+                  {[
+                    "Kinyarwanda alphabet",
+                    "Numbers",
+                    "Transport",
+                    "Cereals",
+                    "Domestic animals",
+                    "Wild animals",
+                    "Aquatic animals",
+                    "Birds",
+                    "Fruits",
+                  ].map((item, i) => (
+                    <div
+                      key={i + 1}
+                      className="col-span-12 md:col-span-6 lg:col-span-6 flex flex-row items-center gap-2 mb-2"
+                    >
+                      <div>
+                        <div className="h-7 w-7 flex items-center justify-center rounded-full bg-primary-800 text-white">
+                          {i + 1}
+                        </div>
+                      </div>
+                      <div>{item}</div>
+                    </div>
+                  ))}
                 </div>
                 <div className="mb-5 text-gray-500 text-justify">
-                  We are a Rwandan Publishing house; passionate about sharing
-                  best books but most importantly motivating and cultivating the
-                  love of reading, with high quality books, for all age ranges
-                  in Kinyarwanda, French, Swahili, and English.
-                </div>
-                <div className="mb-5 text-gray-500 text-justify">
-                  We publish and avail quality books which offer holistic and
-                  innovative educational ends and promote the culture of
-                  reading. We believe that reading helps in exercising the
-                  brains and we are here to assist you achieve this.
+                  <Link
+                    href={"/contact"}
+                    className="font-bold hover:text-primary-800"
+                  >
+                    Contact us
+                  </Link>{" "}
+                  if you would like to place an order.
                 </div>
                 <div className="flex flex-col md:flex-row md:items-center gap-3 mt-5">
                   <Link
